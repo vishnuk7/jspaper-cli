335bb99a36e845dfe75ee19e02b95655f2a54bb1
vishnuk7 <vishnu.kraman88@gmail.com>
2021-03-27 02:58:21 +0530

    create proxy 😻

--- packages/cli/commands/serve.ts
+++ packages/cli/commands/serve.ts
@@ -6,7 +6,16 @@ export const serveCommand = new Command()
 	.command('serve [filename]')
 	.description('Open a file for editing 📝')
 	.option('-p, --port <number>', 'port to run server on', '4005')
-	.action((filename = 'notebook.js', options: { port: string }) => {
-		const dir = path.join(process.cwd(), path.dirname(filename));
-		serve(parseInt(options.port), path.basename(filename), dir);
+	.action(async (filename = 'notebook.js', options: { port: string }) => {
+		try {
+			const dir = path.join(process.cwd(), path.dirname(filename));
+			await serve(parseInt(options.port), path.basename(filename), dir);
+			console.log(`Opened ${filename}.Navigate to http://localhost:${options.port} to edit the file`);
+		} catch (error) {
+			if (error.code === 'EADDRINUSE') {
+				console.log('Port is in use');
+			}
+			console.log(error.message);
+			process.exit(1);
+		}
 	});
